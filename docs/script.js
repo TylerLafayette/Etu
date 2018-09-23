c82ef7df6b27b83da1561ebbade5f956e7978717
Alex <alexanderjwaitz@gmail.com>
2018-09-23 03:02:06 -0700

    New additions/improvements to the map

--- docs/script.js
+++ docs/script.js
@@ -18,26 +18,21 @@ var map = new H.Map(document.getElementById('map'),
     defaultLayers.normal.map, { pixelRatio: pixelRatio });
 
 // giving the map interactivity
-var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+//var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
 // default interactivity ui components
-var ui = H.ui.UI.createDefault(map, defaultLayers);
+//var ui = H.ui.UI.createDefault(map, defaultLayers);
 
 // getting current location and rendering it to the map
 function success(position) {
-    map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude })
+    map.setCenter({ lat: 37.5486678, lng: -122.05929580000002 })
     map.setZoom(14)
 
     // creating markers
-    var svgMarkup = '<svg width="24" height="24" ' +
-        'xmlns="http://www.w3.org/2000/svg">' +
-        '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
-        'height="22" /><text x="12" y="18" font-size="12pt" ' +
-        'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
-        'fill="white">H</text></svg>'
+    var svgMarkup = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/" x="0px" y="0px" width="100px" height="100px" viewBox="0 0 391.7 472.9" style="enable-background:new 0 0 391.7 472.9;" xml:space="preserve"><style type="text/css">.st0{fill:#00E9BA;} .st1{fill:none;stroke:#000000;stroke-width:19.9029;stroke-miterlimit:10;}</style><defs></defs><path class="st0" d="M334.4,334.4L195.9,472.9L57.4,334.4c-76.5-76.5-76.5-200.5,0-277l0,0c76.5-76.5,200.5-76.5,277,0l0,0C410.9,133.9,410.9,257.9,334.4,334.4z"/><g><path class="st1" d="M129.7,214.1c0,37,30,67,67,67h0c37,0,67-30,67-67"/><circle cx="126.6" cy="99.7" r="12.7"/><circle cx="265.1" cy="99.5" r="12.7"/><polyline class="st1" points="195.5,121 195.5,186.3 228.5,186.3"/></g></svg>'
 
     var icon = new H.map.Icon(svgMarkup),
-        coords = { lat: position.coords.latitude, lng: position.coords.longitude },
+        coords = { lat: 37.5486678, lng: -122.05929580000002 },
         marker = new H.map.Marker(coords, { icon: icon });
 
     map.addObject(marker);
